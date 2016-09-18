cd7757adefaa7ad381fc213cff35eb8ea6b7448e
jarradbattaglia <jmbbat@gmail.com>
2016-09-18 12:41:19 -0700

    Add app components and all compiled files

---
+++ angular2-quickstart/app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>My Second Angular 2 App</h1> <p> Adding something new! </p>'
+})
+export class AppComponent { }
